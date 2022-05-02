51556c7952f32a4696d9ec49d6ced2a7af08d1a3
MNastri <marcellonastri@gmail.com>
2022-05-02 14:20:19 -0300

    feat:implement __eq__ for Move

--- move.py
+++ move.py
@@ -10,3 +10,12 @@ class Move(NamedTuple):
     center: Disc = None
     perimeter: List[Disc] = [None] * 6
     remaining_discs: List[Disc] = [None]
+
+    def __eq__(self, other):
+        if (
+            self.center == other.center
+            and self.perimeter == other.perimeter
+            and self.remaining_discs == other.remaining_discs
+        ):
+            return True
+        return False
